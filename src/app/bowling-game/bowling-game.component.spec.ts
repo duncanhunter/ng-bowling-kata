a04c2d30f4693ae3b93f222e044da4920c08e226
Duncan Hunter <duncan@duncanhunter.com.au>
2018-01-11 09:49:37 +0100

    up to a spare

--- src/app/bowling-game/bowling-game.component.spec.ts
+++ src/app/bowling-game/bowling-game.component.spec.ts
@@ -1,6 +1,5 @@
+import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { BowlingGameComponent } from './bowling-game.component';
-import { ComponentFixture } from '@angular/core/testing';
-import { TestBed } from '@angular/core/testing';
 
 describe(`Bowling Game`, () => {
   let component: BowlingGameComponent;
@@ -15,42 +14,85 @@ describe(`Bowling Game`, () => {
     component = fixture.componentInstance;
   });
 
-  it(`should return true`, () => {
+  it(`should have component instance`, () => {
     expect(component).toBeTruthy();
   });
 
-  it(`should have 0 score for a gutter game`, () => {
-    rollMany(20, 0);
+  it(`should have a score of 0 for a gutter game`, () => {
+    component.rollMany(20, 0);
     expect(component.score()).toEqual(0);
   });
 
-  it(`should return 20 for all 1's`, () => {
-    rollMany(20, 1);
-
+  it(`should have all ones`, () => {
+    component.rollMany(20, 1);
     expect(component.score()).toEqual(20);
   });
 
-  fit(`should test one spare`, () => {
+  // TODO: up to slide 46 anf the fourth test after making a spare
+  it(`should have a spare `, () => {
     rollSpare();
     component.roll(3);
-    rollMany(17, 0);
+    component.rollMany(17, 0);
     expect(component.score()).toEqual(16);
   });
-
-  function rollMany(n: number, pins: number): void {
-    for (let i = 0; i < n; i++) {
-      component.roll(pins);
-    }
-  }
 
   function rollSpare() {
     component.roll(5);
     component.roll(5);
   }
-
 });
 
+// import { BowlingGameComponent } from './bowling-game.component';
+// import { ComponentFixture } from '@angular/core/testing';
+// import { TestBed } from '@angular/core/testing';
 
+// describe(`Bowling Game`, () => {
+//   let component: BowlingGameComponent;
+//   let fixture: ComponentFixture<BowlingGameComponent>;
+
+//   beforeEach(() => {
+//     TestBed.configureTestingModule({
+//       declarations: [BowlingGameComponent]
+//     });
+
+//     fixture = TestBed.createComponent(BowlingGameComponent);
+//     component = fixture.componentInstance;
+//   });
+
+//   it(`should return true`, () => {
+//     expect(component).toBeTruthy();
+//   });
+
+//   it(`should have 0 score for a gutter game`, () => {
+//     rollMany(20, 0);
+//     expect(component.score()).toEqual(0);
+//   });
+
+//   it(`should return 20 for all 1's`, () => {
+//     rollMany(20, 1);
+
+//     expect(component.score()).toEqual(20);
+//   });
+
+//   fit(`should test one spare`, () => {
+//     rollSpare();
+//     component.roll(3);
+//     rollMany(17, 0);
+//     expect(component.score()).toEqual(16);
+//   });
+
+//   function rollMany(n: number, pins: number): void {
+//     for (let i = 0; i < n; i++) {
+//       component.roll(pins);
+//     }
+//   }
+
+//   function rollSpare() {
+//     component.roll(5);
+//     component.roll(5);
+//   }
+
+// });
 
 // import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
--- src/app/bowling-game/bowling-game.component.ts
+++ src/app/bowling-game/bowling-game.component.ts
@@ -2,24 +2,22 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-bowling-game',
-  templateUrl: './bowling-game.component.html',
-  styles: [require('src/app/bowling-game/bowling-game.component.scss').toString()]
+  templateUrl: 'bowling-game.component.html'
 })
 export class BowlingGameComponent {
-  rolls = [] as [number];
+  // score = 0;
+  rolls = [] as number[];
   currentRoll = 0;
+  constructor() {}
 
-  roll(pins: number): void {
-    this.rolls[this.currentRoll++] = pins;
-  }
-
-  score(): number {
+  score() {
     let score = 0;
     let frameIndex = 0;
     for (let frame = 0; frame < 10; frame++) {
       if (this.isSpare(frameIndex)) {
         score += 10 + this.rolls[frameIndex + 2];
         frameIndex += 2;
+        console.log(frameIndex);
       } else {
         score += this.rolls[frameIndex] + this.rolls[frameIndex + 1];
         frameIndex += 2;
@@ -28,12 +26,57 @@ export class BowlingGameComponent {
     return score;
   }
 
+  roll(pins: number) {
+    this.rolls = [...this.rolls, pins];
+  }
+
   private isSpare(frameIndex: number): boolean {
     return this.rolls[frameIndex] + this.rolls[frameIndex + 1] === 10;
   }
 
+  rollMany(n: number, pins: number) {
+    for (let i = 0; i < n; i++) {
+      this.currentRoll++;
+      this.roll(pins);
+    }
+  }
 }
 
+// import { Component, OnInit } from '@angular/core';
+
+// @Component({
+//   selector: 'app-bowling-game',
+//   templateUrl: './bowling-game.component.html',
+//   styles: [require('src/app/bowling-game/bowling-game.component.scss').toString()]
+// })
+// export class BowlingGameComponent {
+//   rolls = [] as [number];
+//   currentRoll = 0;
+
+//   roll(pins: number): void {
+//     this.rolls[this.currentRoll++] = pins;
+//   }
+
+//   score(): number {
+//     let score = 0;
+//     let frameIndex = 0;
+//     for (let frame = 0; frame < 10; frame++) {
+//       if (this.isSpare(frameIndex)) {
+//         score += 10 + this.rolls[frameIndex + 2];
+//         frameIndex += 2;
+//       } else {
+//         score += this.rolls[frameIndex] + this.rolls[frameIndex + 1];
+//         frameIndex += 2;
+//       }
+//     }
+//     return score;
+//   }
+
+//   private isSpare(frameIndex: number): boolean {
+//     return this.rolls[frameIndex] + this.rolls[frameIndex + 1] === 10;
+//   }
+
+// }
 
 // totalScore = 0;
 
@@ -50,4 +93,3 @@ export class BowlingGameComponent {
 //     this.roll(pins);
 //   }
 // }
-
